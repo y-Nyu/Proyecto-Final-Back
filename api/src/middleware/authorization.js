85f4e2428183c052bfabf4f18252965a4776f1ac
GreenCheeseWheel <thegreencheesewheel@gmail.com>
2023-09-23 17:04:19 -0300

    fix(client) Prisma client is now brought from db.js file

--- api/src/middleware/authorization.js
+++ api/src/middleware/authorization.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require("@prisma/client")
+const PrismaClient = require("../db");
 const bcrypt = require("bcrypt");
 const client = new PrismaClient();
 const {ADMIN_PAGES} = require("../utils/utils")
